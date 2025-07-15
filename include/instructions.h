b0be3cbbb2a1c63d8b10f55a8ffe930939c3d28a
Max Hoffer <max.hofferlormand@gmail.com>
2025-07-15 21:58:51 +0200

    Adding function declaration for op

---
+++ include/instructions.h
@@ -0,0 +1,83 @@
+#include "cpu.h"
+
+//0x00XX
+void op_00e0(cpu* proc);
+
+void op_00ee(cpu* proc);
+
+//
+void op_1000(cpu* proc);
+
+//
+void op_2000(cpu* proc);
+
+//
+void op_3000(cpu* proc);
+
+//
+void op_4000(cpu* proc);
+
+//
+void op_5000(cpu* proc);
+
+//
+void op_6000(cpu* proc);
+
+//
+void op_7000(cpu* proc);
+
+//0x8XX
+void op_8001(cpu* proc);
+
+void op_8002(cpu* proc);
+
+void op_8003(cpu* proc);
+
+void op_8004(cpu* proc);
+
+void op_8005(cpu* proc);
+
+void op_8006(cpu* proc);
+
+void op_8007(cpu* proc);
+
+void op_800e(cpu* proc);
+
+//
+void op_9000(cpu* proc);
+
+//
+void op_a000(cpu* proc);
+
+//
+void op_b000(cpu* proc);
+
+//
+void op_c000(cpu* proc);
+
+//
+void op_d000(cpu* proc);
+
+//OxEXX
+void op_e09e(cpu* proc);
+
+void op_e0a1(cpu* proc);
+
+// OxFXX
+void op_f007(cpu* proc);
+
+void op_f00a(cpu* proc);
+
+void op_f015(cpu* proc);
+
+void op_f018(cpu* proc);
+
+void op_f01e(cpu* proc);
+
+void op_f029(cpu* proc);
+
+void op_f033(cpu* proc);
+
+void op_f055(cpu* proc);
+
+void op_f065(cpu* proc);
