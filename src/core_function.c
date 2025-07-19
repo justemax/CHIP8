5135df69269f80a0819a1438927dfb5a17da62b4
Max Hoffer <max.hofferlormand@gmail.com>
2025-07-19 16:39:12 +0200

    Adding function call to core function

--- src/core_function.c
+++ src/core_function.c
@@ -1,5 +1,5 @@
 #include "../include/core_function.h"
-
+#include "../include/instructions.h"
 
 void init_system(cpu* proc)
 {
@@ -88,59 +88,76 @@ void cycle_emulation(cpu* proc)
 			switch(proc->opcode & 0x00FF)
 			{
 				case 0x00E0: //Clear screen
+					op_00e0(proc);
 					break;
 				case 0x00EE: //Return from subroutine
+					op_00ee(proc);
 					break;
 				default:
 					printf("UNKNOWN 0x0XXX OPCODE: %x", proc->opcode);
 			}
 
 		case 0x1000: //jump to addr
+			op_1000(proc);
 			break;
 
 		case 0x2000: //call subroutine
+			op_2000(proc);
 			break;
 	
 		case 0x3000:
+			op_3000(proc);
 			break;
 
 		case 0x4000:
+			op_4000(proc);
 			break;
 
 		case 0x5000:
+			op_5000(proc);
 			break;
 
 		case 0x6000:
+			op_6000(proc);
 			break;
 
 		case 0x7000:
+			op_7000(proc);
 			break;
 
 		case 0x8000:
 			switch(proc->opcode & 0x000F)
 			{
-				case 0x001:
+				case 0x8001:
+					op_8001(proc);
 					break;
 
-				case 0x002:
+				case 0x8002:
+					op_8002(proc);
 					break;
 
-				case 0x003:
+				case 0x8003:
+					op_8003(proc);
 					break;
 
-				case 0x004:
+				case 0x8004:
+					op_8004(proc);
 					break;
 
-				case 0x005:
+				case 0x8005:
+					op_8005(proc);
 					break;
 
-				case 0x006:
+				case 0x8006:
+					op_8006(proc);
 					break;
 
-				case 0x007:
+				case 0x8007:
+					op_8007(proc);
 					break;
 
-				case 0x00E:
+				case 0x800E:
+					op_800e(proc);
 					break;
 
 				default:
@@ -148,27 +165,34 @@ void cycle_emulation(cpu* proc)
 			}
 
 		case 0x9000:
+			op_9000(proc);
 			break;
 
 		case 0xA000:
+			op_a000(proc);
 			break;
 
 		case 0xB000:
+			op_b000(proc);
 			break;
 
 		case 0xC000:
+			op_c000(proc);
 			break;
 
 		case 0xD000:
+			op_d000(proc);
 			break;
 
 		case 0xE000:
 			switch(proc->opcode & 0x00FF)
 			{
 				case 0x009E:
+					op_e09e(proc);
 					break;
 
 				case 0x00A1:
+					op_e0a1(proc);
 					break;
 
 				default:
@@ -179,37 +203,46 @@ void cycle_emulation(cpu* proc)
 			switch(proc->opcode & 0x00FF)
 			{
 				case 0x0007:
+					op_f007(proc);
 					break;
 
 				case 0x000A:
+					op_f00a(proc);
 					break;
 
 
 				case 0x0015:
+					op_f015(proc);
 					break;
 
 
 				case 0x0018:
+					op_f018(proc);
 					break;
 
 
 				case 0x001E:
+					op_f01e(proc);
 					break;
 
 
 				case 0x0029:
+					op_f029(proc);
 					break;
 
 
 				case 0x0033:
+					op_f033(proc);
 					break;
 
 
 				case 0x0055:
+					op_f055(proc);
 					break;
 
 
 				case 0x0065:
+					op_f065(proc);
 					break;
 
 				default:
